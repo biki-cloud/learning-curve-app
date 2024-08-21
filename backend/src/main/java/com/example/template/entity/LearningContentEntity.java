0a602e71d2b72b8e5147effe5d7921e37e693d53
hibiki <hibiki123123@gmail.com>
2024-08-21 20:44:14 +0900

    change readme

--- backend/src/main/java/com/example/template/entity/LearningContentEntity.java
+++ backend/src/main/java/com/example/template/entity/LearningContentEntity.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Table;
+import lombok.Data;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "learning_content")
+@Data
 public class LearningContentEntity {
 
     @Id
@@ -33,53 +35,4 @@ public class LearningContentEntity {
 
     @Column(nullable = true)
     private LocalDate createdDate;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
-
-    public LocalDate getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDate createdDate) {
-        this.createdDate = createdDate;
-    }
 }
