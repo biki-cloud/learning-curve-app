2c176ef2efec9c6b8f0636fea30f4cf78398bbad
hibiki <hibiki123123@gmail.com>
2024-08-21 18:57:36 +0900

    change readme

--- backend/src/main/java/com/example/template/config/CorsConfig.java
+++ backend/src/main/java/com/example/template/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
