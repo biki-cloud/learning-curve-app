63b16be2c240b3f6b1e9994409ab4caffc262ede
hibiki <hibiki123123@gmail.com>
2024-08-23 20:11:19 +0900

    change readme

--- backend/src/main/java/com/example/template/entity/UserEntity.java
+++ backend/src/main/java/com/example/template/entity/UserEntity.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class UserEntity {
 
     @Id
@@ -23,37 +25,4 @@ public class UserEntity {
 
     @Column(nullable = false)
     private String email;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
