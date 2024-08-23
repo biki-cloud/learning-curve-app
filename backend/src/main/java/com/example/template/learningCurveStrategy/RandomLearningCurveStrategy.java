dd62fa1ed7a648ea59979b98a01f64be0e5b2adc
hibiki <hibiki123123@gmail.com>
2024-08-23 20:15:24 +0900

    change readme

--- backend/src/main/java/com/example/template/learningCurveStrategy/RandomLearningCurveStrategy.java
+++ backend/src/main/java/com/example/template/learningCurveStrategy/RandomLearningCurveStrategy.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@Primary
 public class RandomLearningCurveStrategy implements LearningCurveStrategy {
 
     @Override
--- backend/src/main/java/com/example/template/learningCurveStrategy/SimpleLearningCurveStrategy.java
+++ backend/src/main/java/com/example/template/learningCurveStrategy/SimpleLearningCurveStrategy.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-@Primary
 public class SimpleLearningCurveStrategy implements LearningCurveStrategy {
 
     @Override
