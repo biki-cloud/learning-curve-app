5d6b72078d651ff4488234aef4293e5e9cd7187f
hibiki <hibiki123123@gmail.com>
2024-09-01 09:35:50 +0900

    change readme

--- frontend/app/learningContent/detail/[id]/page.tsx
+++ frontend/app/learningContent/detail/[id]/page.tsx
@@ -31,12 +31,14 @@ export default function DetailLearningContent({ params }: Props) {
   }
 
   return (
-    <div className="p-8 space-y-8">
+    <div className="p-8 space-y-8 bg-gray-50 min-h-screen">
       <header className="text-center mb-8">
-        <h1 className="text-4xl font-bold mb-4">学習内容の詳細</h1>
+        <h1 className="text-4xl font-bold mb-4 text-blue-600">
+          学習内容の詳細
+        </h1>
       </header>
       <main>
-        <section className="bg-white border border-gray-300 rounded-lg p-6 shadow-sm mb-8">
+        <section className="bg-white border border-gray-300 rounded-lg p-6 shadow-lg mb-8">
           <h2 className="text-2xl font-semibold mb-2">タイトル</h2>
           <p className="text-xl text-gray-800 mb-4">{learningContent.title}</p>
           <h3 className="text-lg font-medium mb-2">カテゴリー</h3>
@@ -47,9 +49,14 @@ export default function DetailLearningContent({ params }: Props) {
           {/* MarkdownPreviewを使ってcontentを表示 */}
           <MarkdownPreview markdownString={learningContent.content} />
         </section>
-        <section className="text-center">
+        <section className="text-center mb-4">
           <Link href={`/learningContent/edit/${id}`} passHref>
-            <Button variant="default">編集</Button>
+            <Button
+              variant="default"
+              className="bg-blue-500 text-white hover:bg-blue-600 transition duration-300"
+            >
+              編集
+            </Button>
           </Link>
         </section>
       </main>
