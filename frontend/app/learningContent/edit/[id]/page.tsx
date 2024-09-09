ae8c032b8d5ddcfe1ec728ed63730366d7c37b95
hibiki <hibiki123123@gmail.com>
2024-09-09 22:09:31 +0900

    change readme

--- frontend/app/learningContent/edit/[id]/page.tsx
+++ frontend/app/learningContent/edit/[id]/page.tsx
@@ -87,71 +87,70 @@ export default function EditLearningContent({ params }: Props) {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="w-full mx-auto p-4 space-y-6 bg-white shadow-lg rounded-lg"
-    >
-      <h2 className="text-2xl font-semibold text-center mb-4">
-        学習内容を編集
-      </h2>
-      <div>
-        <label htmlFor="title" className="block text-sm font-medium mb-1">
-          タイトル
-        </label>
-        <input
-          id="title"
-          type="text"
-          name="title"
-          value={learningContent.title}
-          onChange={(e) => handleChange("title", e.target.value)}
-          className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-      <div>
-        <label htmlFor="content" className="block text-sm font-medium mb-1">
-          内容
-        </label>
-        <div className="flex">
-          <div className="w-1/2">
-            <SimpleMDE
-              value={learningContent.content}
-              onChange={(value) => handleChange("content", value)}
-              events={{ drop: handleDrop }}
-            />
-          </div>
-          <div className="w-1/2 pl-4">
-            <MarkdownPreview markdownString={learningContent.content} />
+    <div className="p-8 space-y-8 bg-white shadow-lg rounded-lg">
+      <header className="text-center mb-4">
+        <h2 className="text-2xl font-semibold">学習内容を編集</h2>
+        <div className="flex justify-center space-x-4 mt-4">
+          <Button
+            type="submit"
+            className="bg-blue-500 text-white hover:bg-blue-600 transition duration-300"
+          >
+            更新
+          </Button>
+          <Button
+            type="button"
+            onClick={handleDelete}
+            className="bg-red-500 text-white hover:bg-red-600 transition duration-300"
+          >
+            削除
+          </Button>
+        </div>
+      </header>
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <div>
+          <label htmlFor="title" className="block text-sm font-medium mb-1">
+            タイトル
+          </label>
+          <input
+            id="title"
+            type="text"
+            name="title"
+            value={learningContent.title}
+            onChange={(e) => handleChange("title", e.target.value)}
+            className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="content" className="block text-sm font-medium mb-1">
+            内容
+          </label>
+          <div className="flex">
+            <div className="w-1/2">
+              <SimpleMDE
+                value={learningContent.content}
+                onChange={(value) => handleChange("content", value)}
+                events={{ drop: handleDrop }}
+              />
+            </div>
+            <div className="w-1/2 pl-4">
+              <MarkdownPreview markdownString={learningContent.content} />
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <label htmlFor="category" className="block text-sm font-medium mb-1">
-          カテゴリ
-        </label>
-        <input
-          id="category"
-          type="text"
-          name="category"
-          value={learningContent.category}
-          onChange={(e) => handleChange("category", e.target.value)}
-          className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-      <div className="text-center">
-        <Button
-          type="submit"
-          className="w-full bg-blue-500 text-white hover:bg-blue-600 transition duration-300"
-        >
-          更新
-        </Button>
-        <Button
-          type="button"
-          onClick={handleDelete}
-          className="mt-4 w-full bg-red-500 text-white hover:bg-red-600 transition duration-300"
-        >
-          削除
-        </Button>
-      </div>
-    </form>
+        <div>
+          <label htmlFor="category" className="block text-sm font-medium mb-1">
+            カテゴリ
+          </label>
+          <input
+            id="category"
+            type="text"
+            name="category"
+            value={learningContent.category}
+            onChange={(e) => handleChange("category", e.target.value)}
+            className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+      </form>
+    </div>
   );
 }
