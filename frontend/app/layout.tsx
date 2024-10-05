6a1aee01afabb91718eaa240f4f793113c619f5f
hibiki <hibiki123123@gmail.com>
2024-10-05 16:33:52 +0900

    change readme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,34 +1,39 @@
-"use client";
-import type { Metadata } from "next";
-import { Toaster } from "@/components/ui/toaster";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import NavBar from "@/components/mylib/NavBar";
-import { ThemeProvider } from "@/components/theme-provider";
-import { useState } from "react";
+import { Metadata } from 'next'
+import '@/app/globals.css'
+import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Lexical Rich Text Editor | Next.js 13',
+  description: 'Prepopulated with daily standup text',
+  keywords: ["lexical", "lexical rich text editor", "daily standup", "next.js", "next.js 13"]
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const [isOpen, setIsOpen] = useState(true); // ナビゲーションバーの状態を管理
-
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider>
-          <div className="flex">
-            <NavBar setIsOpen={setIsOpen} /> {/* NavBarに状態を渡す */}
-            <main className={`flex-1 p-4 ${isOpen ? "ml-64" : "ml-0"}`}>
-              {children}
-            </main>
-            <Toaster />
-          </div>
-        </ThemeProvider>
-      </body>
+    <html lang="en">
+      <head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S5Z6JM69TE"
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S5Z6JM69TE');
+          `}
+        </Script>
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
---
+++ frontend/app/text-daily-standup.tsx
@@ -0,0 +1,49 @@
+import { format } from "date-fns";
+import { $createParagraphNode, $createTextNode, $getRoot } from "lexical";
+import { $createListItemNode, $createListNode } from "@lexical/list";
+
+/**
+ * docs: https://lexical.dev/docs/demos/plugins/markdown
+ */
+
+export function textDailyStandup() {
+  const root = $getRoot();
+
+  /**
+   * Today's Standup date
+   */
+  const paragraphParent = $createParagraphNode();
+  const currentDate = new Date();
+  const todayStandupDate = format(currentDate, "ddMMyyyy");
+  paragraphParent.append($createTextNode(`/${todayStandupDate}`));
+
+  const paragraphTwo = $createParagraphNode();
+  paragraphTwo.append($createTextNode("#focusing"));
+  const listForParagraphTwo = $createListNode("bullet");
+  listForParagraphTwo.append(
+    $createListItemNode().append($createTextNode(`..`))
+  );
+
+  const paragraphThree = $createParagraphNode();
+  paragraphThree.append($createTextNode("#queue"));
+  const listForParagraphThree = $createListNode("bullet");
+  listForParagraphThree.append(
+    $createListItemNode().append($createTextNode(`..`))
+  );
+
+  const paragraphFour = $createParagraphNode();
+  paragraphFour.append($createTextNode("#achieved"));
+  const listForParagraphFour = $createListNode("bullet");
+  listForParagraphFour.append(
+    $createListItemNode().append($createTextNode(`..`))
+  );
+
+  /**
+   * Root
+   */
+  root.append(paragraphParent);
+  root.append(paragraphTwo);
+  root.append(listForParagraphTwo);
+  root.append(paragraphThree);
+  root.append(paragraphFour);
+}
